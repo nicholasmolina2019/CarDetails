e18494737f3dbda0f86d7ee4fc1af61fea0eda6c
Nick Molina <104439965+nicholasmolina2019@users.noreply.github.com>
2022-07-12 09:57:02 -0500

    fixed db

--- carDjango/carDjango/settings.py
+++ carDjango/carDjango/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'carDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cardetail',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
